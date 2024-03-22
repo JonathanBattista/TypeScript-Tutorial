9828d21694fda20a594a1f3c200400af72fe763c
JonathanBattista <145586415+JonathanBattista@users.noreply.github.com>
2024-03-21 23:15:25 -0400

    continued learning on variable types

--- 01intro/variableMe.js
+++ 01intro/variableMe.js
@@ -1,2 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Jon";
+var mynum = 6;
+mynum.toLowerCase();
+greetings.toLowercase();
 console.log(greetings);
