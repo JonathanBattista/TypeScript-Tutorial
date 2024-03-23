5aecae828b8599287bff046c2e40984653a43969
JonathanBattista <145586415+JonathanBattista@users.noreply.github.com>
2024-03-22 22:20:04 -0400

    added any variable

--- 01intro/variableMe.ts
+++ 01intro/variableMe.ts
@@ -1,11 +1,33 @@
+//Typsescript is smart enough to assign values to variables and tell you when you can or cannot use functions or methods for said variables
+
+//string
+
 let greetings: string = "Jon";
-
-let mynum = 6;
-
-mynum.toLowerCase();
 
 greetings.toLowercase();
 
 console.log(greetings);
+
+// number
+
+let userId = 334455.3;
+
+userId.toFixed();
+//number = 334455.3
+//userId = 27
+
+// boolean
+
+let isLoggedIn: boolean = false;
+
+// any
+
+let hero;
+
+function getHero() {
+  return "thor";
+}
+
+hero = getHero();
 
 export {};
