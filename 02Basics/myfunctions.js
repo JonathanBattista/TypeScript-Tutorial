77f045a842146bcba5c35747b33b825b43f325c0
JonathanBattista <145586415+JonathanBattista@users.noreply.github.com>
2024-03-25 22:52:11 -0400

    void, never

--- 02Basics/myfunctions.js
+++ 02Basics/myfunctions.js
@@ -7,8 +7,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // addTwo("5");
 // export {};
 function addTwo(num) {
-    //return num + 2;
-    return "hello";
+    return num + 2; // -correct / simple example
+    //return "hello"; - solved by asking for a second check on number
 }
 function getUpper(val) {
     return val.toUpperCase();
@@ -17,8 +17,35 @@ function signUpUser(name, email, isPaid) { }
 var loginUser = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
 };
-//addTwo(5);
-var myValue = addTwo(5);
+addTwo(5); //-correct / simple example
+//let myValue = addTwo(5); - complicated possible problem
 getUpper("jon");
 signUpUser("jon", "jon@me.com", false);
 loginUser("h", "h@h.com");
+// multiple types
+// function getValue(myVal: number)  {
+//   if (myVal > 5) {
+//     return true
+//   }
+//   return "200 ok"
+// }
+// A function whose declared type is neither 'undefined', 'void', nor 'any' must return a value.
+// const getHello = (s: string): string => {
+//   return " "
+// }
+// typescript will automatically deduce what type is coming from an array
+var heroes = ["thor", "spiderman", "ironman"];
+//const heroes = [1, 2, 3]
+heroes.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+//void
+function consoleError(errmsg) {
+    console.log(errmsg);
+    //return 1
+}
+// never
+function handleError(errmsg) {
+    throw new Error(errmsg);
+    //return 1
+}
--- 02Basics/myfunctions.ts
+++ 02Basics/myfunctions.ts
@@ -7,9 +7,9 @@
 
 // export {};
 
-function addTwo(num: number) {
-  //return num + 2;
-  return "hello";
+function addTwo(num: number): number {
+  return num + 2; // -correct / simple example
+  //return "hello"; - solved by asking for a second check on number
 }
 
 function getUpper(val: string) {
@@ -20,10 +20,47 @@ function signUpUser(name: string, email: string, isPaid: boolean) {}
 
 let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
 
-//addTwo(5);
-let myValue = addTwo(5);
+addTwo(5); //-correct / simple example
+//let myValue = addTwo(5); - complicated possible problem
 getUpper("jon");
 signUpUser("jon", "jon@me.com", false);
 loginUser("h", "h@h.com");
+
+// multiple types
+
+// function getValue(myVal: number)  {
+//   if (myVal > 5) {
+//     return true
+//   }
+//   return "200 ok"
+// }
+
+// A function whose declared type is neither 'undefined', 'void', nor 'any' must return a value.
+
+// const getHello = (s: string): string => {
+//   return " "
+// }
+
+// typescript will automatically deduce what type is coming from an array
+const heroes = ["thor", "spiderman", "ironman"];
+//const heroes = [1, 2, 3]
+
+heroes.map((hero): string => {
+  return `hero is ${hero}`;
+});
+
+//void
+
+function consoleError(errmsg: string): void {
+  console.log(errmsg);
+  //return 1
+}
+
+// never
+
+function handleError(errmsg: string): never {
+  throw new Error(errmsg);
+  //return 1
+}
 
 export {};
