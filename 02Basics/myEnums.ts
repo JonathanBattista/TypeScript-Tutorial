8366372c5ba3805e35a1663fafa8e8524c306c4c
JonathanBattista <145586415+JonathanBattista@users.noreply.github.com>
2024-03-27 17:26:10 -0400

    added enum

---
+++ 02Basics/myEnums.ts
@@ -0,0 +1,18 @@
+// const AISLE = 0
+// const MIDDLE = 1
+// const WINDOW = 2
+
+// if (seat === 0) {
+
+// }
+// CONST enum SeatChoice will only generate necessary code
+enum SeatChoice {
+  //always starts with 0,1,2 ect
+  // you can change the value
+  AISLE = "aisle",
+  MIDDLE = "middle",
+  WINDOW = "window",
+  FOURTH = 0,
+}
+
+const hcSeat = SeatChoice.AISLE;
