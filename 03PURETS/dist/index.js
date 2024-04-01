aeb0e3619fd099ea27b465f6450aae724a309419
JonathanBattista <145586415+JonathanBattista@users.noreply.github.com>
2024-03-31 23:35:40 -0400

    project classes

--- 03PURETS/dist/index.js
+++ 03PURETS/dist/index.js
@@ -1,3 +1,10 @@
 "use strict";
-console.log("typescript is here still");
-console.log("typescript is amazing");
+class User {
+    constructor(email, name) {
+        this.city = "Balt";
+        this.email = email;
+        this.name = name;
+    }
+}
+const jon = new User("jon@jon.com", "jon");
+//jon.city = "balt";
--- 03PURETS/src/index.ts
+++ 03PURETS/src/index.ts
@@ -1,2 +1,12 @@
-console.log("typescript is here still");
-console.log("typescript is amazing");
+class User {
+  email: string;
+  name: string;
+  readonly city: string = "Balt";
+  constructor(email: string, name: string) {
+    this.email = email;
+    this.name = name;
+  }
+}
+
+const jon = new User("jon@jon.com", "jon");
+//jon.city = "balt";
