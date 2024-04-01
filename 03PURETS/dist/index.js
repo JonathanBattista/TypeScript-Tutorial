81fff7d844ca12f8bbdfbe95bffe209921354e7a
JonathanBattista <145586415+JonathanBattista@users.noreply.github.com>
2024-03-31 23:43:16 -0400

    added public and private examples

--- 03PURETS/dist/index.js
+++ 03PURETS/dist/index.js
@@ -1,10 +1,22 @@
 "use strict";
+// class User {
+//   public email: string;
+//   private name: string;
+//   //#name: string; another way to write private but only used in JS
+//   readonly city: string = "Balt";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+//const jon = new User("jon@jon.com", "jon");
+//jon.city = "balt";
+// better way to wrie it and introducing the public and priavte keyword
 class User {
     constructor(email, name) {
-        this.city = "Balt";
         this.email = email;
         this.name = name;
+        this.city = "Balt";
     }
 }
 const jon = new User("jon@jon.com", "jon");
-//jon.city = "balt";
--- 03PURETS/src/index.ts
+++ 03PURETS/src/index.ts
@@ -1,12 +1,25 @@
+// class User {
+//   public email: string;
+//   private name: string;
+//   //#name: string; another way to write private but only used in JS
+//   readonly city: string = "Balt";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
+//const jon = new User("jon@jon.com", "jon");
+//jon.city = "balt";
+
+// better way to wrie it and introducing the public and priavte keyword
 class User {
-  email: string;
-  name: string;
   readonly city: string = "Balt";
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
-  }
+  constructor(
+    public email: string,
+    public name: string
+  ) // private userId: string
+  {}
 }
 
 const jon = new User("jon@jon.com", "jon");
-//jon.city = "balt";
