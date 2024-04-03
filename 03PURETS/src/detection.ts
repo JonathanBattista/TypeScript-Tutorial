b2950cc3892a54414674e738ab25400cf043ab2d
JonathanBattista <145586415+JonathanBattista@users.noreply.github.com>
2024-04-02 23:56:34 -0400

    added exhaustive chekcing and discriminated unions

--- 03PURETS/src/detection.ts
+++ 03PURETS/src/detection.ts
@@ -70,3 +70,78 @@ function getFood(pet: Fish | Bird) {
     return "bird food";
   }
 }
+
+//discriminated unions
+
+//hacky way fo doing it
+// interface Circle {
+//     kind: "circle"
+// }
+
+// interface square {
+//     kind: "square"
+// }
+
+interface circle {
+  kind: "circle";
+  radius: number;
+}
+
+interface square {
+  kind: "square";
+  side: number;
+}
+
+interface rectangle {
+  kind: "rectangle";
+  length: number;
+  width: number;
+}
+
+// example of exhaustive checking
+
+// type Shape = circle | square
+
+// function getTrueShape(shape: Shape){
+//     if(shape.kind === "circle"){
+//         return Math.PI * shape.radius **2
+//     }
+//     return shape.side * shape.side
+// }
+
+// function getArea(shape: Shape){
+//     switch(shape.kind){
+//         case "circle":
+//             return Math.PI * shape.radius **2
+
+//         case "square":
+//             return shape.side * shape.side
+//     }
+// }
+
+// example of exhaustive checking after adding rectangle to our shape type
+type Shape = circle | square | rectangle;
+
+function getTrueShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  }
+  //return shape.side * shape.side
+}
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+
+    case "square":
+      return shape.side * shape.side;
+
+    case "rectangle":
+      return shape.length * shape.width;
+
+    default:
+      const _defaultforshape: never = shape;
+      return _defaultforshape;
+  }
+}
