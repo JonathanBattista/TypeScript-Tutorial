99fb397c5677ee9fc22e6ec95251fb154acc211a
JonathanBattista <145586415+JonathanBattista@users.noreply.github.com>
2024-04-02 23:27:11 -0400

    added narrowing

---
+++ 03PURETS/dist/detection.js
@@ -0,0 +1,27 @@
+"use strict";
+function detectTypes(val) {
+    if (typeof val === "string") {
+        return val.toLowerCase();
+    }
+    return val + 3;
+}
+function provideId(id) {
+    if (!id) {
+        console.log("please provide id");
+        return;
+    }
+    id.toLowerCase;
+}
+//example of way of doing it but does not cover empty arrays
+function printAll(strs) {
+    if (strs) {
+        if (typeof strs === "object") {
+            for (const s of strs) {
+                console.log(s);
+            }
+        }
+        else if (typeof strs === "string") {
+            console.log(strs);
+        }
+    }
+}
---
+++ 03PURETS/src/detection.ts
@@ -0,0 +1,27 @@
+function detectTypes(val: number | string) {
+  if (typeof val === "string") {
+    return val.toLowerCase();
+  }
+  return val + 3;
+}
+
+function provideId(id: string | null) {
+  if (!id) {
+    console.log("please provide id");
+    return;
+  }
+  id.toLowerCase;
+}
+
+//example of way of doing it but does not cover empty arrays
+function printAll(strs: string | string[] | null) {
+  if (strs) {
+    if (typeof strs === "object") {
+      for (const s of strs) {
+        console.log(s);
+      }
+    } else if (typeof strs === "string") {
+      console.log(strs);
+    }
+  }
+}
